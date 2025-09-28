945777c2579a040c165f5b1b34d803bcb87ee5b8
christoesper <christopher.chukwu007@gmail.com>
2025-09-28 18:09:35 +0100

    Add files via upload

---
+++ data_types/test.py
@@ -0,0 +1,6 @@
+#Challenge: Create a program that asks for two numbers and prints the sum.
+
+num_1 = int(input("what number would you like to add: "))
+num_2 = int(input("select a second number to add to the first: "))
+sum  = num_1 + num_2
+print(f"{num_1} + {num_2} = {sum}")
